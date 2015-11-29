9551f7df83e4eb5a93f955cdeea11f119847a113
Oren Novotny <oren@novotny.org>
2015-11-29 14:51:58 -0500

    Update proj names and add test proj for uwp runner app

---
+++ test/test.xunit.extensions.uitest.uwp.runner/UWPRunnerTests.cs
@@ -0,0 +1,44 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Xunit;
+
+
+
+public class UWPRunnerTests
+{
+    [UIFact]
+    public async void DoSomethingOnUIThread()
+    {
+        var ele = new SearchBox();
+        ele.Visibility = Visibility.Collapsed;
+
+        ele.QueryText = "foo";
+
+        Assert.Equal("foo", ele.QueryText);
+
+        await Task.Delay(20);
+
+        ele.QueryText = "bar";
+        Assert.Equal("bar", ele.QueryText);
+    }
+
+    [UITheory]
+    [InlineData("foo")]
+    [InlineData("FooBar")]
+    [InlineData("Bar")]
+    public void UITheory(string value)
+    {
+        var ele = new SearchBox();
+        ele.Visibility = Visibility.Collapsed;
+        ele.QueryText = value;
+
+
+        Assert.Equal(3, ele.QueryText.Length);
+    }
+
+}
