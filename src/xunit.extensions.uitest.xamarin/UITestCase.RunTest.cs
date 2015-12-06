8a24112d1e9985d06fe9b41230bd9b3c2d48e8ea
Oren Novotny <oren@novotny.org>
2015-12-06 16:29:10 -0500

    Refactor UITestCase

---
+++ src/xunit.extensions.uitest.xamarin/UITestCase.RunTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Xunit.Extensions.UITest
+{
+    partial class UITestCase
+    {
+        void RunTestImpl(IMessageSink diagnosticMessageSink, IMessageBus messageBus, object[] constructorArguments, ExceptionAggregator aggregator, CancellationTokenSource cancellationTokenSource, TaskCompletionSource<RunSummary> tcs)
+        {
+
+#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
+            // Run on the UI thread
+            Device.BeginInvokeOnMainThread(
+                () =>
+                {
+                    try
+                    {
+                        var result = testCase.RunAsync(diagnosticMessageSink, messageBus, constructorArguments, aggregator, cancellationTokenSource);
+                        result.ContinueWith(t =>
+                        {
+                            if (t.IsFaulted)
+                                tcs.SetException(t.Exception);
+
+                            tcs.SetResult(t.Result);
+                        });
+                    }
+                    catch (Exception e)
+                    {
+                        tcs.SetException(e);
+                    }
+                });
+#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+
+        }
+    }
+}
--- src/xunit.extensions.uitest/UITestCase.cs
+++ src/xunit.extensions.uitest/UITestCase.cs
@@ -5,14 +5,12 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Windows.ApplicationModel.Core;
-using Windows.UI.Core;
 using Xunit.Abstractions;
 using Xunit.Sdk;
 
 namespace Xunit.Extensions.UITest
 {
-    class UITestCase : IXunitTestCase
+    partial class UITestCase : IXunitTestCase
     {
         IXunitTestCase testCase;
 
@@ -54,37 +52,9 @@ namespace Xunit.Extensions.UITest
 
         public Task<RunSummary> RunAsync(IMessageSink diagnosticMessageSink, IMessageBus messageBus, object[] constructorArguments, ExceptionAggregator aggregator, CancellationTokenSource cancellationTokenSource)
         {
-
-            //var disp = SecondaryView.Dispatcher;
-            var disp = CoreApplication.MainView.Dispatcher;
-
             var tcs = new TaskCompletionSource<RunSummary>();
 
-
-
-#pragma warning disable CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
-                              // Run on the current window's dispatcher
-            disp.RunAsync(CoreDispatcherPriority.Normal,
-                () =>
-                {
-                    try
-                    {
-                        var result = testCase.RunAsync(diagnosticMessageSink, messageBus, constructorArguments, aggregator, cancellationTokenSource);
-                        result.ContinueWith(t =>
-                                            {
-                                                if (t.IsFaulted)
-                                                    tcs.SetException(t.Exception);
-
-                                                tcs.SetResult(t.Result);
-                                            });
-                    }
-                    catch (Exception e)
-                    {
-                        tcs.SetException(e);
-                    }
-
-                });
-#pragma warning restore CS4014 // Because this call is not awaited, execution of the current method continues before the call is completed
+            RunTestImpl(diagnosticMessageSink, messageBus, constructorArguments, aggregator, cancellationTokenSource, tcs);
 
             return tcs.Task;
         }
