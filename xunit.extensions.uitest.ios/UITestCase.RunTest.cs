cc5dfb1ee8333ff7e4b3f440f19b9617d336e6a8
Oren Novotny <oren@novotny.org>
2016-03-05 13:46:36 -0500

    Add iOS UI fact

---
+++ xunit.extensions.uitest.ios/UITestCase.RunTest.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using Foundation;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Xunit.Extensions.UITest
+{
+    partial class UITestCase
+    {
+        void RunTestImpl(IMessageSink diagnosticMessageSink, IMessageBus messageBus, object[] constructorArguments, ExceptionAggregator aggregator, CancellationTokenSource cancellationTokenSource, TaskCompletionSource<RunSummary> tcs)
+        {
+            // Run on the UI thread
+             NSRunLoop.Main.BeginInvokeOnMainThread(
+                () =>
+                {
+                    try
+                    {
+                        var result = testCase.RunAsync(diagnosticMessageSink, messageBus, constructorArguments, aggregator, cancellationTokenSource);
+                        result.ContinueWith(t =>
+                        {
+                            if (t.IsFaulted)
+                                tcs.SetException(t.Exception);
+
+                            tcs.SetResult(t.Result);
+                        });
+                    }
+                    catch (Exception e)
+                    {
+                        tcs.SetException(e);
+                    }
+                });
+        }
+    }
+}
