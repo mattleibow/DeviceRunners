408212bf37d42be3acf4f96710ab000dc754d789
Rob Prouse <rob@prouse.org>
2018-03-19 21:01:21 -0400

    UWP now compiling and running. Requries Fall Creator's update

--- tests/nunit.runner.tests.uwp/App.xaml.cs
+++ tests/nunit.runner.tests.uwp/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿// Copyright (c) 2015 CNUnit Project
+﻿// Copyright (c) 2018 NUnit Project
 //
 // Permission is hereby granted, free of charge, to any person obtaining
 // a copy of this software and associated documentation files (the
@@ -40,9 +40,6 @@ namespace NUnit.Runner.Tests
         /// </summary>
         public App()
         {
-            Microsoft.ApplicationInsights.WindowsAppInitializer.InitializeAsync(
-                Microsoft.ApplicationInsights.WindowsCollectors.Metadata |
-                Microsoft.ApplicationInsights.WindowsCollectors.Session);
             this.InitializeComponent();
             this.Suspending += OnSuspending;
         }
@@ -54,14 +51,6 @@ namespace NUnit.Runner.Tests
         /// <param name="e">Details about the launch request and process.</param>
         protected override void OnLaunched(LaunchActivatedEventArgs e)
         {
-
-#if DEBUG
-            if (System.Diagnostics.Debugger.IsAttached)
-            {
-                this.DebugSettings.EnableFrameRateCounter = true;
-            }
-#endif
-
             Frame rootFrame = Window.Current.Content as Frame;
 
             // Do not repeat app initialization when the Window already has content,
