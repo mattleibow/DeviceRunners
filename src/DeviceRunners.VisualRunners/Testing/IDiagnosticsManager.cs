e14622129f2618167da92484cfbd6f0ac83da628
Matthew Leibowitz <mattleibow@live.com>
2024-03-13 05:02:45 +0200

    Update to .NET 8 (#22)

--- src/DeviceRunners.VisualRunners/Testing/IDiagnosticsManager.cs
+++ src/DeviceRunners.VisualRunners/Testing/IDiagnosticsManager.cs
@@ -4,5 +4,5 @@ public interface IDiagnosticsManager
 {
 	void PostDiagnosticMessage(string message);
 
-	event EventHandler<string>? DiagnosticMessageRecieved;
+	event EventHandler<string>? DiagnosticMessageReceived;
 }
